d2ff08152f46bd511861ee0b701ec9bbca0faddd
Askhat Bikmetov <lolborgini@gmail.com>
2018-08-28 02:17:19 +0300

    Fix http semantics

--- agent/src/index.js
+++ agent/src/index.js
@@ -1,36 +1,30 @@
 import express from 'express'
+import compression from 'compression'
+import apicache from 'apicache'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import ls from './lib/ls'
-import unzip from './lib/unzip'
 import wc from './lib/wc'
-import { PORT, ORIGINS } from './settings'
+import { PORT } from './settings'
+
 
 export const tmpZips = new Map
 
 const app = express()
-app.use(morgan('dev'))
+// FIXME
+app.use(cors({ origin: (origin, cb) => cb(null, true) }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors({
-  origin: (origin, cb) => {
-    if (ORIGINS.includes(origin)) {
-      cb(null, true)
-    } else {
-      // FIXME
-      // cb(new Error('Restricted by CORS'))
-      cb(null, true)
-    }
-  }
-}))
-app.listen(PORT, () => {
-  console.info(`Agent is running at http://localhost:${PORT}/`)
-})
+app.use(compression())
+app.use(apicache.middleware('5 minutes'))
+app.use(morgan('dev'))
 
-app.put('/ls', async (req, res) => {
+app.listen(PORT, () => console.info(`Agent is running at http://localhost:${PORT}/`))
+
+app.get('/ls', async (req, res) => {
   try {
-    const { path } = req.body
+    const { path } = req.query
     const data = await ls(path)
     res.json(data)
   } catch (e) {
@@ -38,9 +32,9 @@ app.put('/ls', async (req, res) => {
   }
 })
 
-app.put('/wc', async (req, res) => {
+app.get('/wc', async (req, res) => {
   try {
-    const { path } = req.body
+    const { path } = req.query
     const data = await wc(path)
     res.json(data)
   } catch (e) {
