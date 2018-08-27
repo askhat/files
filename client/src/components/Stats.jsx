b4de36b9f6da2bd0557e4ca176ec301c51168120
Askhat Bikmetov <askhat.bikmetov@akvelon.com>
2018-08-27 17:03:11 +0300

    squash: toolbar & chart types

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -1,41 +1,19 @@
 import React, { Component } from 'react'
-import { Segment, Dimmer, Loader, Menu, Checkbox, Dropdown, Popup } from 'semantic-ui-react'
+import { Dimmer, Loader } from 'semantic-ui-react'
 import Chart from 'react-google-charts'
 
 export default class Stats extends Component {
-  state = {
-    currentChartType: 'BarChart',
-    chartTypes: ['BarChart', 'ColumnChart', 'PieChart']
-  }
-
-  switchChartType = (e, { value: currentChartType }) => {
-    this.setState({ currentChartType })
-  }
-
   render = () => {
-    const { state } = this
-    return (<Segment basic>
-      <Menu secondary>
-        <Popup content='Select all files in current directory' trigger={<Menu.Item>
-          <Checkbox label='Overall' onChange={this.props.onSelectOverall}/>
-        </Menu.Item>}/>
-        <Menu.Item>
-          <Dropdown
-            text={state.currentChartType}
-            onChange={this.switchChartType}
-            options={state.chartTypes.map(o => ({text: o, value: o}))}/>
-        </Menu.Item>
-      </Menu>
-      <Chart
-        key={state.currentChartType}
-        chartType={state.currentChartType}
-        loader={<Dimmer active inverted><Loader>Stats&nbsp;are&nbsp;loading</Loader></Dimmer>}
-        options={{ title: this.props.title, legend: false }}
-        height='80vh'
-        data={[
-          ['Word', 'Occurences'],
-          ...this.props.data
-        ]}/>
-    </Segment>)
+    const { props } = this
+    return (<Chart
+      key={props.chartType}
+      chartType={props.chartType}
+      loader={<Dimmer active inverted><Loader>Stats&nbsp;are&nbsp;loading</Loader></Dimmer>}
+      options={{ title: props.title, legend: false }}
+      height='80vh'
+      data={[
+        ['Word', 'Occurences'],
+        ...props.data
+      ]}/>)
   }
 }
--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -2,25 +2,38 @@ import React, { Component } from 'react'
 import { Menu, Icon, Input, Checkbox, Dropdown, Popup } from 'semantic-ui-react'
 
 export default class Toolbar extends Component {
+  switchChartType = (e, { value: currentChartType }) => {
+    this.setState({ currentChartType })
+  }
+
   render = () => {
+    const { props } = this
+
     return (<Menu>
-      <Popup content='Click to get back' trigger={<Menu.Item>
-        <Icon name='arrow left'/>
-      </Menu.Item>}/>
-      <Popup content='Edit path then press return to navagate' trigger={<Menu.Item>
-        <Input transparent className='icon' icon='arrow right'/>
-      </Menu.Item>}/>
-      <Menu.Menu position='right'>
-        <Popup content='Include files in subdirectories' trigger={<Menu.Item>
-          <Checkbox label='Recursive'/>
+      <Popup
+        content='Click to get back'
+        trigger={<Menu.Item>
+          <Icon name='arrow left'/>
         </Menu.Item>}/>
-        <Popup content='Show overall direcory stats' trigger={<Menu.Item>
-          Overall
+
+      <Popup
+        content='Edit path then press return to navagate'
+        trigger={<Menu.Item
+          style={{flexGrow: 1}}
+          as='div'>
+          <Input
+            transparent
+            className='icon'
+            icon='arrow right'
+            value={this.props.path}/>
         </Menu.Item>}/>
-        <Menu.Item>
-            <Dropdown text='Chart type'/>
-        </Menu.Item>
-      </Menu.Menu>
+
+      <Menu.Item>
+          <Dropdown
+            text={props.chartType}
+            options={props.chartTypes.map(o => ({text: o, value: o}))}
+            onChange={props.onSwitchChartType}/>
+      </Menu.Item>
     </Menu>)
   }
 }
