073361537b48d32c7891827dd3106a3fb0d4559e
Askhat Bikmetov <askhat.bikmetov@akvelon.com>
2018-08-27 17:02:35 +0300

    Add file checkboxes

--- client/src/components/Files.jsx
+++ client/src/components/Files.jsx
@@ -1,29 +1,54 @@
 import React, { Component } from 'react'
-import { List } from 'semantic-ui-react'
+import { List, Checkbox } from 'semantic-ui-react'
 import fileIcon from '../lib/file_icon'
 
 export default class Files extends Component {
+  showCheckbox = ({ currentTarget }) => {
+    currentTarget.childNodes.forEach(node => console.log(node))
+  }
+
   render = () => {
     return (<List divided relaxed>
-      {this.props.canGoBack && <List.Item onDoubleClick={() => {
-          this.props.onSelectItem({ type: 'directory', path: '..'})
+      {this.props.canGoBack && <List.Item
+        onDoubleClick={() => {
+            this.props.onSelectItem({ type: 'directory', path: '..'})
         }}>
-        <List.Icon name='folder open' size='large' verticalAlign='middle'/>
+        <List.Icon
+          name='folder open'
+          size='large'
+          verticalAlign='middle'/>
         <List.Content>
           <List.Header>..</List.Header>
           <List.Description>Back to the parent directory</List.Description>
         </List.Content>
       </List.Item>}
+
       {this.props.listing.map(file => {
-        return (<List.Item key={file.path}
+        return (<List.Item
+            key={file.path}
             onDoubleClick={() => this.props.onSelectItem(file)}>
-          <List.Icon name={fileIcon(file.type)} size='large' verticalAlign='middle'/>
-          <List.Content>
-            <List.Header>{file.name}</List.Header>
-            <List.Description>{file.type}</List.Description>
-          </List.Content>
+          <div style={flex}>
+            <div style={flex}>
+              <List.Icon
+                name={fileIcon(file.type)}
+                size='large'
+                verticalAlign='middle'/>
+              <List.Content>
+                <List.Header>{file.name}</List.Header>
+                <List.Description>{file.type}</List.Description>
+              </List.Content>
+            </div>
+            <Checkbox/>
+          </div>
         </List.Item>)
       })}
     </List>)
   }
+}
+
+const flex = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center'
 }
